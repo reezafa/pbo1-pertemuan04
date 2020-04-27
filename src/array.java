6e91cf0a1f12d214854e08a256da2afbab968fd7
reezafa <a.fakhreeza@gmail.com>
2020-04-27 18:07:00 +0800

    array total hari pada bulan

---
+++ src/array.java
@@ -0,0 +1,21 @@
+public class array {
+    public static void main(String[] args) {
+        int [] jumlahHari;
+        jumlahHari = new int[12];
+        jumlahHari[0] = 31;
+        jumlahHari[1] = 28;
+        jumlahHari[2] = 31;
+        jumlahHari[3] = 30;
+        jumlahHari[4] = 31;
+        jumlahHari[5] = 30;
+        jumlahHari[6] = 31;
+        jumlahHari[7] = 31;
+        jumlahHari[8] = 30;
+        jumlahHari[9] = 31;
+        jumlahHari[10] = 30;
+        jumlahHari[11] = 31;
+
+        System.out.println("Bulan Maret memiliki " + jumlahHari[2] + " hari.");
+
+    }
+}
